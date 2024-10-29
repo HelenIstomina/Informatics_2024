ad04a3fec0cdc7c6b7bf0952811afb89eb93dd13
HelenIstomina <elenka.ies@gmail.com>
2024-10-29 20:18:31 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -9,9 +9,7 @@ const a float64 = 1.1
 const b float64 = 0.09
 
 func main() {
-	fmt.Println("Task A:")
-	lab4.TaskA(a,b,1.2,2.2,0.2)
-	fmt.Println("Task B:")
+	fmt.Println(lab4.TaskA(a,b,1.2,2.2,0.2))
 	var s = [5]float64{1.21,1.76,2.53,3.48,4.52}
-	lab4.TaskB(a, b, s)
+	fmt.Println(lab4.TaskB(a, b, s))
 }
