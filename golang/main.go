7e5d44f76d2e1ee58f48dbbc7f1da662d7d735a7
HelenIstomina <elenka.ies@gmail.com>
2024-10-30 13:39:19 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -12,6 +12,8 @@ func main() {
 	fmt.Println(lab4.TaskA(a,b,1.2,2.2,0.2))
 	var s = [5]float64{1.21,1.76,2.53,3.48,4.52}
 	fmt.Println(lab4.TaskB(a, b, s))
+
+	fmt.Println("Istomina_Elena_Sergeevna")
 }
 
-fmt.Println("Istomina_Elena_Sergeevna")
+
