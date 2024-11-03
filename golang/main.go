1662bc21a86ac8ba6862574a84f6534e11fd3a44
HelenIstomina <elenka.ies@gmail.com>
2024-11-03 17:20:14 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"isuct.ru/informatics2022/lab4"
-	"isuct.ru/informatics2022/lab6"
 	"fmt"
 )
 
 func main() {
 	Lab4.RunLab4()
-	Lab6.RunLab6()
+	Lab4.RunLab6()
 
 	fmt.Println("Istomina_Elena_Sergeevna")
 }
