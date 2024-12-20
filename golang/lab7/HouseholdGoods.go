fc87ec070b69d81f4ea9a12678a9e83285c279d8
HelenIstomina <elenka.ies@gmail.com>
2024-12-20 10:36:51 +0300

    Update lab7

--- golang/lab7/HouseholdGoods.go
+++ golang/lab7/HouseholdGoods.go
@@ -2,27 +2,27 @@ package lab7
 
 import "fmt"
 
-type HouseholdGoods struct {
+type Microwave_oven struct {
 	Name      string
 	Price     float64
 }
 
-func (f HouseholdGoods) GetDescription() {
+func (f Microwave_oven) GetDescription() {
 	fmt.Println("Микроволновая печь", f.Name, "стоимостью", f.Price)
 }
 
-func (f HouseholdGoods) GetPrice() float64 {
+func (f Microwave_oven) GetPrice() float64 {
 	return f.Price
 }
 
-func (f *HouseholdGoods) ApplyDiscount(x float64) {
+func (f *Microwave_oven) ApplyDiscount(x float64) {
 	(*f).Price = (f.Price) * (x)
 }
 
-func (f *HouseholdGoods) SetNewPrice(x float64) {
+func (f *Microwave_oven) SetNewPrice(x float64) {
 	(*f).Price = x
 }
 
-func (f *HouseholdGoods) SetNewDescription(x string) {
+func (f *Microwave_oven) SetNewDescription(x string) {
 	(*f).Name = x
 }
